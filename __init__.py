e946d55e9a8b1c0be12b10088264ef1876251517
Ildanin <ilya.a.danilenko@gmail.com>
2026-02-19 02:18:18 +0300

    add a sortcut for using random_array function

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
-from .networkClass import Network, load
-from .postProcess import add_noise, apply_threshold
-from .activators import add_activator, Activators
+from network.networkClass import Network, load
+from network.postProcess import add_noise, apply_threshold
+from network.activators import add_activator, Activators
+from network.utiles import random_array
