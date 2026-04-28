8f901f85cb738ae5acc8b912ccb0a7fd5a34a1f9
ildanin <ilya.a.danilenko@gmail.com>
2026-04-28 22:38:30 +0300

    refactor: overwrite __getitem__ method

--- datasetClass.py
+++ datasetClass.py
@@ -1,4 +1,5 @@
 from numpy import ndarray
+from typing import overload
 
 class DataSample:
     def __init__(self, input_value: ndarray, output_value: ndarray) -> None:
@@ -8,3 +9,10 @@ class DataSample:
 class Dataset(list):
     def __init__(self, input_values: list[ndarray] = [], output_values: list[ndarray] = []) -> None:
         super().__init__(DataSample(input_value, output_value) for input_value, output_value in zip(input_values, output_values))
+    
+    @overload
+    def __getitem__(self, key: int) -> DataSample: ...
+    @overload
+    def __getitem__(self, key: slice) -> list[DataSample]: ...
+    def __getitem__(self, key: int | slice) -> DataSample | list[DataSample]:
+        return super().__getitem__(key)
