2f969760710234de37b6d0ee0ba14cea7be1633e
ildanin <ilya.a.danilenko@gmail.com>
2026-04-28 23:50:34 +0300

    fix: add __iadd__ function

--- gradientClass.py
+++ gradientClass.py
@@ -1,14 +1,20 @@
-from typing import Iterable, Self
+from typing import Iterator, Iterable, Self
 from .layerClass import Layer
 
 class Gradient(list):
     def __init__(self, layers: Iterable[Layer] = []) -> None:
         super().__init__(layers)
     
+    def __iter__(self) -> Iterator[Layer]:
+        return super().__iter__()
+
     def __add__(self, gradient: Self) -> Self:
         for i, layer in enumerate(gradient):
             self[i] += layer
         return self
+    
+    def __iadd__(self, gradient: Self) -> Self:
+        return self.__add__(gradient)
     
     def __mul__(self, coef: float) -> Self:
         for i, layer in enumerate(self):
