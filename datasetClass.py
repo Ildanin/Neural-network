0481f9d1c019473e96a9b8532bc59d9969597630
ildanin <ilya.a.danilenko@gmail.com>
2026-06-01 01:16:28 +0300

    refactor: add type hinting for dataset iteration

--- datasetClass.py
+++ datasetClass.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 from numpy import ndarray
 from typing import overload
 
@@ -9,6 +10,9 @@ class DataSample:
 class Dataset(list):
     def __init__(self, input_values: list[ndarray] = [], output_values: list[ndarray] = []) -> None:
         super().__init__(DataSample(input_value, output_value) for input_value, output_value in zip(input_values, output_values))
+    
+    def __iter__(self) -> Iterator[DataSample]:
+        return super().__iter__()
     
     @overload
     def __getitem__(self, key: int) -> DataSample: ...
